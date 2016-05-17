8261ce5d4b1957d33efb68e2c61e411d87fda1af
Mike Zamansky <zamansky@gmail.com>
2016-05-17 14:33:19 -0400

    Fixed Readme.org link and added org-bullets to init

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key try use-package))))
+ '(package-selected-packages (quote (org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,3 +38,9 @@
 	:config
 	(which-key-mode))
 
+
+;; org-mode stuff
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
