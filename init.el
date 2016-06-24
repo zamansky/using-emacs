da72e4f1144281c0d624f500ebcc450f2fe4ee2d
Mike Zamansky <zamansky@gmail.com>
2016-06-24 14:09:21 -0400

    did themes

--- init.el
+++ init.el
@@ -1,20 +1,5 @@
 ;; stuff automatically added
 ;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auto-complete counsel swiper lorem-ipsum tabbar ace-window org-bullets which-key try use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-
 
 ;; Our stuff
 
@@ -116,3 +101,10 @@
     (ac-config-default)
     (global-auto-complete-mode t)
     ))
+
+(use-package color-theme
+  :ensure t)
+
+(use-package zenburn-theme
+  :ensure t
+  :config (load-theme 'zenburn t))
