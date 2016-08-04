444b826842691f0ca372e9e089d34cbefa4f57ce
Mike Zamansky <zamansky@gmail.com>
2016-08-04 15:25:59 -0400

    13 yasnippet

--- samples/python/test.py
+++ samples/python/test.py
@@ -1,10 +1,24 @@
 import random
 
 def f(x):
-    return random.randrange(1,x)
+    return random.randrange(1, x)
 
 
 for i in range(2,10):
     print f(i)
     
             
+
+if __name__ == '__main__':
+    hello
+
+
+class myclass(somethingelse):
+    """documentation
+
+    """
+    def __init__(self, some_args):
+        super(myclass, self).__init__()
+        self.some_args = some_args
+        
+        
