8254f1d1cfacd4744f03ff088f88e6aa53d1e8ab
Mike Zamansky <zamansky@gmail.com>
2019-05-20 07:54:31 -0400

    added posframe

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(hydra-hint-display-type 'posframe)
  '(org-confirm-babel-evaluate nil)
  '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/Sync/orgfiles")
